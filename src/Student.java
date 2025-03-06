5c8b255145949ee26205f32ce3753160940593e9
Fred Morrison <frederick.morrison@outlook.com>
2025-03-06 16:24:08 -0700

    add ability to track courses taken by a student

--- src/Student.java
+++ src/Student.java
@@ -1,7 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Student extends Person {
 
-
     private int gradeLevel;
+    private List<Course> courses = new ArrayList<Course>();
 
     private Student() {/* prevent uninitialized instances */}
     public Student(String firstName, String lastName, int gradeLevel) {
@@ -10,6 +13,9 @@ public class Student extends Person {
         this.gradeLevel = gradeLevel;
     }
 
+    public int getGradeLevel() {return gradeLevel;}
+    public List<Course> getCourses() {return courses;}
+
     @Override
     public String toString() {
         return new StringBuilder("Student{")
@@ -17,6 +23,10 @@ public class Student extends Person {
                 .append(", firstName='").append(firstName).append('\'')
                 .append(", gradeLevel=").append(gradeLevel)
                 .append('}').toString();
+    }
+
+    public void addCourse(Course course) {
+        courses.add(course);
     }
 
 }
