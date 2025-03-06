aba3a61d6bcd1e3a0a637108313ea77e09b4a3ad
Fred Morrison <frederick.morrison@outlook.com>
2025-03-06 16:48:55 -0700

    create a Course and show it in a department, teacher and student.

--- src/Main.java
+++ src/Main.java
@@ -3,23 +3,26 @@ import java.util.Collections;
 public class Main {
     public static void main(String[] args) {
 
+        Course c1 = new Course("Java", "Java Programming");
+        System.out.format("%s\n", c1);
+
         Department d1 = new Department("Computer Science", "Computer Science & Cryptography");
         System.out.format("%s\n", d1);
+        d1.addCourse(c1);
 
         Student s1 = new Student("Mythical", "Last-Name", 12);
         System.out.format("%s\n", s1);
+        s1.addCourse(c1);
 
         Teacher mgrohman = new Teacher( "Mark", "Grohman");
         System.out.format("%s\n", mgrohman);
-
-        Course c1 = new Course("Java", "Java Programming");
-        System.out.format("%s\n", c1);
+        mgrohman.addCourse(c1);
 
         School lphs = new School("Lewis-Palmer","LPHS", "1300 Highby Road", "Monument" );
         System.out.format("%s\n", lphs);
         lphs.addTeacher(mgrohman);
         lphs.addStudent(s1);
-        lphs.addCourse(c1);
+        lphs.addCourse(c1); // MIGHT WANT TO KEEP THIS ONLY IN DEPARTMENT
         lphs.addDepartment(d1);
 
         School prhs = new School("Palmer Ridge", "PRHS", "19255 Monument Hill Road", "Monument" );
