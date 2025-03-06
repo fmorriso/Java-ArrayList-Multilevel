508ba61da4397e9221bfb70b51cba08d9ab6633c
Fred Morrison <frederick.morrison@outlook.com>
2025-03-06 12:48:45 -0700

    create more than one school instance

--- src/Main.java
+++ src/Main.java
@@ -6,9 +6,13 @@ public class Main {
         School lphs = new School("Lewis-Palmer","LPHS", "1300 Highby Road", "Monument" );
         System.out.format("%s\n", lphs);
 
+        School prhs = new School("Palmer Ridge", "PRHS", "19255 Monument Hill Road", "Monument" );
+        System.out.format("%s\n", prhs);
+
         District lpsd = new District("Lewis-Palmer", "D38", "146 N. Jefferson Street", "Monument");
         System.out.format("%s\n", lpsd);
         lpsd.addSchool(lphs);
+        lpsd.addSchool(prhs);
 
         colorado.addSchoolDistrict(lpsd);
 
