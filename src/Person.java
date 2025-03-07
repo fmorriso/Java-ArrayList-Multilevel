746abbbfe2d831c22815650199a6f3530ebcccfb
Fred Morrison <frederick.morrison@outlook.com>
2025-03-06 17:03:18 -0700

    add getters for first and last name.

--- src/Person.java
+++ src/Person.java
@@ -3,6 +3,9 @@ public abstract class Person {
     protected String firstName;
     protected String lastName;
 
+    public String getFirstName() {return firstName;}
+    public String getLastName() {return lastName;}
+
     public String fullNameFirstLast() {
         return String.format("%s %s", firstName, lastName);
     }
